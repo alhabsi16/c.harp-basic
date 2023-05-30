50f1bff3e9125fd71884b2cb6e06980f5146c2ba
alhabsi16 <mxmx8283@gmail.com>
2023-05-30 11:25:49 +0400

    Names

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Muhanad");
+        Console.WriteLine("Father Name: Saif");
+        Console.WriteLine("DOB: 26/04/1997");
+        Console.WriteLine("Email: mxmx8283@gmail.com");
+        Console.WriteLine("Githup Username: alhabsi16");
+        Console.Read();
+    }
+}
